62954d6afe6ee88128d01e6a463d72ec70dab7f4
ITSurgeon <stookal@gmail.com>
2023-05-25 11:12:24 +0300

    corrected comment module

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentService } from './services/comment/comment.service';
 import { CommentResolver } from './resolvers/post/comment.resolver';
 import { PostService } from 'src/posts/services/post/post.service';
+import { PostsModule } from 'src/posts/posts.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentEntity])],
-  providers: [CommentService, CommentResolver, PostService],
+  imports: [TypeOrmModule.forFeature([CommentEntity]), PostsModule],
+  providers: [CommentService, CommentResolver, PostsModule],
 })
 export class CommentsModule {}
